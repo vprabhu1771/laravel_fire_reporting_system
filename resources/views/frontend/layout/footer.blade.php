78414d7e1e8dc1474e9189e39bbb7b59b2e5c65c
dhivyasreekr <dhivyasreekr@gmail.com>
2024-09-09 13:54:27 +0530

    Update footer.blade.php

---
+++ resources/views/frontend/layout/footer.blade.php
@@ -0,0 +1,13 @@
+<footer style="background-color: #f1f1f1; padding: 20px; text-align: center;">
+    <p>&copy; <?= date('Y') ?> Fire Report App. All Rights Reserved.</p>
+    <p>
+        <a href="terms.php">Terms of Service</a> | 
+        <a href="privacy.php">Privacy Policy</a> |
+        <a href="contact.php">Contact Us</a>
+    </p>
+    <div>
+        <a href="https://facebook.com" target="_blank">Facebook</a> |
+        <a href="https://twitter.com" target="_blank">Twitter</a> |
+        <a href="https://instagram.com" target="_blank">Instagram</a>
+    </div>
+</footer>
