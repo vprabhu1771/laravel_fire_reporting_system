2bd07db3afd391e5b3b9e0e65620546261f7fd1d
dhivyasreekr <dhivyasreekr@gmail.com>
2024-09-09 12:41:53 +0530

    fire report model and migration

---
+++ database/migrations/2024_09_09_070732_create_fire_reports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fire_reports', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+
+            $table->string('mobile_no');
+
+            $table->string('location')->nullable();
+
+            $table->string('message')->nullable();
+
+            $table->string('status')->default(FireReportStatus::REQUEST_RECEIVED);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fire_reports');
+    }
+};
