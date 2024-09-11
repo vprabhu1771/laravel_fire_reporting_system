4411215061c5f57a47bfb3934ddd09909cdee6e9
vprabhu1771 <vprabhu1771@gmail.com>
2024-09-11 17:28:14 +0530

    OneSIgnal SDK Config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
+        $middleware->alias([
+            'OneSignal' => Berkayk\OneSignal\OneSignalFacade::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
